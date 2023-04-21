e949725ebb7b92a947b6f097fd2f99fecc477cfa
Yuriy Epikhin <epih96@mail.ru>
2023-04-21 07:50:56 +0300

    linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from brain_games.cli import welcome_user 
+from brain_games.cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!')
@@ -8,5 +9,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
